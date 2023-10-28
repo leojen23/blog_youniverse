77a406ccfa56652003681250e79ec951a95f3399
leojen23 <leojen23@hotmail.com>
2023-10-28 14:42:26 +0200

    displays a readable list of users in terminal

--- src/Command/ListUsersCommand.php
+++ src/Command/ListUsersCommand.php
@@ -19,7 +19,7 @@ class ListUsersCommand extends Command
 {
 
     private $em;
-    protected static $defaultName = 'app:list-users';
+    // protected static $defaultName = 'app:list-users';
 
     public function __construct(EntityManagerInterface $em)
     {
@@ -30,21 +30,32 @@ class ListUsersCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('List all users')
-            ->setHelp('This command allows you to list all users in the database');
+            ->setDescription('Liste des utilisateur')
+            ->setHelp('Cette Commande vous permet de lister tous les utilisateur de la base de donéea');
 
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
     $userRepository = $this->em->getRepository(User::class);
-
     $users = $userRepository->findAll();
     
+    $output->writeln(
+        ['Liste des utilisateurs ',
+        '',
+        '--------------------------------']
+    );
     foreach ($users as $user) {
-        $output->writeln($user->getEmail());
-    }
 
+        $rolesToString = implode(' | ',$user->getRoles());
+        $output->writeln([
+            'id:' . $user->getId(),
+            'email: ' . $user->getEmail(),
+            'roles: ' . $rolesToString,
+            '--------------------------------'
+        ]);
+    }
+   
         return Command::SUCCESS;
     }
 }
